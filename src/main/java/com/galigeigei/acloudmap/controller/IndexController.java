32651e501c61d00015d5df99f5ac4e447f704a65
47sang <zhoushiyu92@gmail.com>
2024-07-27 11:40:08 +0800

    每个label显示涨跌幅

--- src/main/java/com/galigeigei/acloudmap/controller/IndexController.java
+++ src/main/java/com/galigeigei/acloudmap/controller/IndexController.java
@@ -1,19 +1,17 @@
 package com.galigeigei.acloudmap.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author 周士钰
  * @date 2024/7/26 下午8:50
  */
-@RestController
+@Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "项目运行成功！"+new Date();
+    @RequestMapping("/")
+    public String html() {
+        return "index.html";
     }
 }
