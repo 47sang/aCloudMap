98a509767809ddb0c78692106b720855cd5bf270
47sang <zhoushiyu92@gmail.com>
2024-10-04 16:07:20 +0800

    调整字体大小

--- src/main/java/com/galigeigei/acloudmap/AcloudmapApplication.java
+++ src/main/java/com/galigeigei/acloudmap/AcloudmapApplication.java
@@ -12,7 +12,10 @@ public class AcloudmapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AcloudmapApplication.class, args);
-        System.out.println("项目启动完成: http://localhost:1808/aCloudMap/");
+        System.out.println("大盘云图: http://localhost:1808/aCloudMap/");
+        System.out.println("市值大小排序: http://localhost:1808/aCloudMap/sort");
+        System.out.println("二级板块云图: http://localhost:1808/aCloudMap/section");
+        System.out.println("二级板块条形图: http://localhost:1808/aCloudMap/sectionBar");
     }
 
 
